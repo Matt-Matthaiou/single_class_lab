00c4644716caa740bf097c1958f7f48dce9c2f18
Matt-Matthaiou <mat.matthaiou@gmail.com>
2022-02-28 13:49:37 +0000

    created the talk method

--- single_class_lab_start_code/student_class/src/student.py
+++ single_class_lab_start_code/student_class/src/student.py
@@ -2,4 +2,9 @@ class Student:
     def __init__(self, name, cohort):
         self.name = name
         self.cohort = cohort
+       
+
+    def talk(self):
+        return "I can talk!"
+        
     
