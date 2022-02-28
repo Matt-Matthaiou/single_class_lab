aa923e25646b071bd0a1b3fcf281c22fb19452bd
Matt-Matthaiou <mat.matthaiou@gmail.com>
2022-02-28 13:57:25 +0000

    created say favourite language method

--- single_class_lab_start_code/student_class/src/student.py
+++ single_class_lab_start_code/student_class/src/student.py
@@ -2,9 +2,12 @@ class Student:
     def __init__(self, name, cohort):
         self.name = name
         self.cohort = cohort
-       
+        self.fav_language = ""
 
     def talk(self):
         return "I can talk!"
         
     
+    def say_favourite_language(self, language):
+        self.fav_language = language
+        return f'I love {self.fav_language}'
